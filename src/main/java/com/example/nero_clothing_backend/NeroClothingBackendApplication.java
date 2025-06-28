049890299ac98de89e0b5701119720abdbc742af
Szymon Wierzchoś <92671269+brexus@users.noreply.github.com>
2025-06-28 19:11:51 +0200

    ✨ initial project setup

---
+++ src/main/java/com/example/nero_clothing_backend/NeroClothingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nero_clothing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeroClothingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeroClothingBackendApplication.class, args);
+	}
+
+}
